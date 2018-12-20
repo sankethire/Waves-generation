d1e5c6a940a060a1b8785a355ef3b721046086b3
sanket hire <sankethire98@gmail.com>
2018-12-20 13:19:14 +0530

    add th square and triangular waveform

---
+++ triangular_wave/sketch.js
@@ -0,0 +1,68 @@
+let time = 0;
+let wave = [];
+
+let slider;
+
+function setup() {
+  createCanvas(1920, 600);
+  slider = createSlider(1,100,1);
+}
+
+function draw() {
+  background(0,0,0);
+  translate(180, 200);
+
+  let x=0;
+  let y=0;
+
+
+  for(let i=0;i<slider.value();i++){
+    let prevx = x;
+    let prevy = y;
+    
+    // let n=2*i+1;
+    // n+=i*(1/20);
+    let n=i+1;
+    if(n%2 != 0){
+      n=-n;
+    }
+  let radius = (2/(n*PI))*75;
+  // let radius = (4/(n*PI))*75;
+
+  
+  x+= radius*cos(n*time);
+  y+= radius*sin(n*time);
+
+
+
+
+  stroke(255,100);
+  noFill();
+  
+  ellipse(prevx,prevy,radius*2);
+
+
+  // fill(255);
+  stroke(255);
+  line(prevx,prevy,x,y);
+  // ellipse(x,y,8);
+
+  
+}
+  wave.unshift(y);
+  translate(200,0);
+  line(x-200,y,0,wave[0]);
+
+  beginShape();
+  noFill();
+  for(let i=0;i<wave.length;i++){
+    vertex(i,wave[i]);
+  }
+  endShape();
+
+  time -= 0.03;
+
+  if(wave.length > 400){
+    wave.pop();
+  }
+}
